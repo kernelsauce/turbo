1bb783440e1dce41c5e55f7ecc86a068ceeaa804
John Abrahamsen <jhnabrhmsn@gmail.com>
2012-02-01 16:40:21 +0100

    New regime :)

--- ExampleUsage.lua
+++ ExampleUsage.lua
@@ -54,7 +54,6 @@ local nonsence = require('nonsence') -- Require nonsence.
 --
 local ExampleHandler = nonsence.web.RequestHandler:new() 
 function ExampleHandler:get(id) -- Handler for GET method requests.
-	dump(id)
 	self:write( { Result = self:get_arguments() } )
 end
 function ExampleHandler:post() -- Handler for POST method requests.
