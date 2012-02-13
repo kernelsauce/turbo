fd1d04414a6cc3476775f41b0aecc25731ec341e
John Abrahamsen <jhnabrhmsn@gmail.com>
2012-02-13 17:28:01 +0100

    Extended TCPServer test

--- nonsence_tcpserver_test.lua
+++ nonsence_tcpserver_test.lua
@@ -46,7 +46,35 @@ local log = assert(require('nonsence_log'),
 	[[Missing nonsence_log module]])
 -------------------------------------------------------------------------
 
-local ioloop = ioloop.instance()
+local io_loop = ioloop.instance()
 local server = tcpserver.TCPServer:new()
+local parse_headers = function(raw_headers)
+	local HTTPHeader = raw_headers
+	if HTTPHeader then
+		-- Fetch HTTP Method.
+		local method, uri = HTTPHeader:match("([%a*%-*]+)%s+(.-)%s")
+		-- Fetch all header values by key and value
+		local request_header_table = {}	
+		for key, value  in HTTPHeader:gmatch("([%a*%-*]+):%s?(.-)[\r?\n]+") do
+			request_header_table[key] = value
+		end
+	return { method = method, uri = uri, extras = request_header_table }
+	end
+end
+
+function server:handle_stream(stream, address)
+	function close()
+		if #io_loop:list_callbacks() > 20 then
+			print(#io_loop:list_callbacks())
+		end
+		stream:close()
+	end	
+	function headers(data)
+		local requestheaders = parse_headers(data)
+		stream:write("HTTP/1.1 200 OK\r" .. "Content-Type: text/html; charset=UTF-8\r" .. "Content-Length: 16\r\n\r\n" .. "TCPServer works!", close)
+	end
+	stream:read_until("\r\n\r\n", headers)
+end
+
 server:listen(8888)
-ioloop:start()
+io_loop:start()
