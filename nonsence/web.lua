9b18a2637ae7d941b73b61ff512020ef15a1cf56
John Abrahamsen <jhnabrhmsn@gmail.com>
2012-03-15 21:02:20 +0100

    First work on the web module

--- nonsence/web.lua
+++ nonsence/web.lua
@@ -28,3 +28,117 @@
 
   ]]
 
+local httpserver = pcall(require, 'httpserver') and require('httpserver') or 
+	error('Missing module httpserver')
+local log = pcall(require, 'log') and require('log') or 
+	error('Missing module log')
+assert(require('middleclass'), 
+	[[Missing required module: MiddleClass 
+	https://github.com/kikito/middleclass]])
+		
+local web = {}
+
+web.RequestHandler = class("RequestHandler")
+--[[
+
+	Request handler class.
+		
+  ]]
+
+function web.RequestHandler:initialize(application, request, kwargs)
+	self.application = application
+	self.request = request
+	self._headers_written = false
+	self._finished = false
+	self._auto_finish = true
+	self._transforms = nil
+	
+	self:clear()
+	
+	local function _on_close_callback()
+		self:on_connection_close()
+	end
+	
+	if self.request:get("Connection") then
+		self.request.connection.stream:set_close_callback(_on_close_callback)
+	end
+	self:on_creation(kwargs)
+end
+
+function web.RequestHandler:on_create(kwargs)
+	--[[
+	 
+		Please redefine this class if you want to do something
+		straight after the class has been created.
+		
+		Parameter can be either a table with kwargs, or a single parameter.
+		
+	  ]]
+end
+
+function web.RequestHandler:head(self, args, kwargs) HTTPError:new(405) end
+function web.RequestHandler:get(self, args, kwargs) HTTPError:new(405) end
+function web.RequestHandler:post(self, args, kwargs) HTTPError:new(405) end
+function web.RequestHandler:delete(self, args, kwargs) HTTPError:new(405) end
+function web.RequestHandler:put(self, args, kwargs) HTTPError:new(405) end
+function web.RequestHandler:options(self, args, kwargs) HTTPError:new(405) end
+
+function web.RequestHandler:prepare()
+	--[[
+	
+		Redefine this method after your likings.
+		Called before get/post/put etc. methods on a request.
+		
+	  ]]
+end
+
+function web.RequestHandler:on_finish()
+	--[[ 
+	
+		Redefine this method after your likings.
+		Called after the end of a request.
+		Usage of this method could be something like a clean up etc.
+		
+	  ]]
+end
+
+function web.RequestHandler:on_connection_close()
+	--[[
+		
+		Called in asynchronous handlers when the connection is closed.
+		Use it to clean up the mess after the connection :-).
+		
+	  ]]
+end
+
+function web.RequestHandler:clear()
+	-- Reset all headers and content for this request.
+	-- TODO make this function :P
+	
+end
+
+function web.RequestHandler:set_default_headers()
+	-- Redefine this method to set HTTP headers at the beginning of
+	-- the request.
+end
+
+function web.RequestHandler:set_status(status_code)
+	-- Sets the status for our response.
+	
+	assert(type(status_code) == "int", [[set_status method requires int.]])
+	self._status_code = status_code
+end
+
+function web.RequestHandler:get_status()
+	-- Returns the status code currently set for our response.
+	
+	return self._status_code
+end
+
+function web.RequestHandler:add_header(key, value)
+	-- Add the given response header key and value to the response.
+
+	self.headers:add(key, value)
+end
+
+return web
