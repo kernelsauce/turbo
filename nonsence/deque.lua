34450931ab78aff7d63701f49422094807d6a7d2
John Abrahamsen <jhnabrhmsn@gmail.com>
2013-04-30 17:51:26 +0200

    Deque is now using a doubly linked list as backend. Big speedup when running deque_test.lua

--- nonsence/deque.lua
+++ nonsence/deque.lua
@@ -22,55 +22,120 @@ SOFTWARE."			]]
 
 
 require('middleclass')
-
+local log = require "log"
+local ffi = require "ffi"
 --[[ Double ended queue class. 	]]
 local deque = class('Deque')
 
 
 function deque:init()
-	self._virtual_queue = {}
+        self.head = nil
+        self.tail = nil
 end
 
 --[[ Append elements to tail.  ]]
-function deque:append(item) self._virtual_queue[#self._virtual_queue + 1] = item end
+function deque:append(item)
+    if (not self.tail) then
+        self.tail = {next = nil, prev = nil, value = item}
+        self.head = self.tail
+    else
+        local old_tail = self.tail
+        local new_tail = {next = nil, prev = old_tail, value = item}
+        old_tail.next = new_tail
+        self.tail = new_tail        
+    end
+end
+
 --[[ Append element to head. 	]]
-function deque:appendleft(item) table.insert(self._virtual_queue, 1, item) end
+function deque:appendleft(item)
+    if (not self.head) then
+        self.head = {next = nil, value = item}
+        self.tail = self.tail
+    else
+        local new_head = {next = self.head, prev = nil, value = item}
+        self.head = new_head        
+    end
+end
 
 --[[ Removes element at tail and returns it. 	]]
 function deque:pop()
-	local len = #self._virtual_queue
-	local pop = self._virtual_queue[len]
-	self._virtual_queue[len] = nil
-	return pop
+    if (not self.tail) then
+        return nil
+    end
+    local value = self.tail.value
+    local new_tail = self.tail.prev
+    if not new_tail then
+        self.tail = nil
+        self.head = nil
+    else
+        self.tail = new_tail
+        new_tail.next = nil
+    end
+        
+    return value
 end
 
 --[[ Removes element at head and returns it. 	]]
 function deque:popleft()
-	local pop = self._virtual_queue[1]
-	table.remove(self._virtual_queue, 1)
-	return pop
+    if (not self.head) then
+        return nil
+    end
+    local value = self.head.value
+    local new_head = self.head.next
+    if (not new_head) then
+        self.head = nil
+        self.tail = nil
+    else
+        new_head.prev = nil
+        self.head = new_head        
+    end
+    return value
+end
+
+function deque:size()
+    if (not self.head) then
+        return 0
+    end
+    local l = self.head
+    local i = 1
+    while (l.next) do
+        i = i + 1
+        l = l.next
+    end
+    return i
+end
+
+function deque:concat()
+    local l = self.head
+    if (not l) then
+        return ""
+    else
+        local sz = 0
+        while (l) do
+            sz = l.value:len() + sz
+            l = l.next            
+        end
+        local buf = ffi.new("char[?]", sz)
+        l = self.head
+        local i = 0
+        while (l) do
+            local len = l.value:len()
+            ffi.copy(buf + i, l.value, len)
+            i = i + len
+            l = l.next        
+        end
+        return ffi.string(buf, sz)
+    end
 end
 
 --[[ Returns element at tail. 	]]
-function deque:peeklast() return self._virtual_queue[#self._virtual_queue] end
+function deque:peeklast() return self.tail.value end
 --[[ Returns element at head. 	]]
-function deque:peekfirst() return self._virtual_queue[1] end
+function deque:peekfirst() return self.head.value end
 
---[[ Returns element at position n. 	]]
-function deque:getn(k)
-	if k == -1 then 
-		return self:peeklast()
-	else
-	return self._virtual_queue[k + 1]
-	end
+
+function deque:not_empty()
+    return self.head ~= nil
 end
-
---[[ Returns size of deque. 	]]
-function deque:size() return #self._virtual_queue or 0 end
---[[ Is deque empty or not?   ]]
-function deque:not_empty() return #self._virtual_queue > 0 and true or false end
-
---[[ Concat all elements. 	]]
-function deque:concat() return table.concat(self._virtual_queue) or '' end
 
 return deque
