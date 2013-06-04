139d3b79d95971636fa1c7b3ad3a80c60aea2b46
John Abrahamsen <jhnabrhmsn@gmail.com>
2013-06-04 23:31:23 +0200

    Fixed indent.

--- examples/httpclient.lua
+++ examples/httpclient.lua
@@ -18,40 +18,40 @@ local turbo = require "turbo"
 local TwitterFeedHandler = class("TwitterFeed", turbo.web.RequestHandler)
 
 function TwitterFeedHandler:get(search)
-	-- Use this handler by directing browser to http://*your hostname*/tweet/*twitter search phrase*
-	local res = coroutine.yield(
-		turbo.async.HTTPClient:new():fetch("http://search.twitter.com/search.json",
-		{
-			params = {
-				q=search,
-				result_type="mixed"
-			}
-		}))
-	
-	if (res.error) then
-		error(turbo.web.HTTPError:new(500, res.error.message))
-	end
-	
-	if (res.headers:get_status_code() == 200) then
-		-- Decode and create nice Twitter page!
-		local json = turbo.escape.json_decode(res.body)
-		self:write(string.format([[<h1>Search for %s on Twitter gave these tweets:</h1>]], search))
-		for k, v in ipairs(json.results) do
-			local text = v.text
-			local pic = v.profile_image_url
-			self:write(string.format([[
-				<img src="%s">
-				<p>%s</p>
-				<br />
-			]], pic, text))
-		end
-	else
-		error(turbo.web.HTTPError:new(res.headers:get_status_code(), "Something bad happened!"))
-	end
+    -- Use this handler by directing browser to http://*your hostname*/tweet/*twitter search phrase*
+    local res = coroutine.yield(
+        turbo.async.HTTPClient:new():fetch("http://search.twitter.com/search.json",
+        {
+            params = {
+                q=search,
+                result_type="mixed"
+            }
+        }))
+    
+    if (res.error) then
+        error(turbo.web.HTTPError:new(500, res.error.message))
+    end
+    
+    if (res.headers:get_status_code() == 200) then
+        -- Decode and create nice Twitter page!
+        local json = turbo.escape.json_decode(res.body)
+        self:write(string.format([[<h1>Search for %s on Twitter gave these tweets:</h1>]], search))
+        for k, v in ipairs(json.results) do
+            local text = v.text
+            local pic = v.profile_image_url
+            self:write(string.format([[
+                <img src="%s">
+                <p>%s</p>
+                <br />
+            ]], pic, text))
+        end
+    else
+        error(turbo.web.HTTPError:new(res.headers:get_status_code(), "Something bad happened!"))
+    end
 end
 
 local application = turbo.web.Application:new({
-	{"/tweet/(.*)", TwitterFeedHandler}
+    {"/tweet/(.*)", TwitterFeedHandler}
 })
 
 application:listen(8888)
