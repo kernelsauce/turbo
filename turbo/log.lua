c58ab340e18f683e372d11af5db483a3d5826716
John Abrahamsen <jhnabrhmsn@gmail.com>
2013-07-02 23:13:55 +0200

    Fixed broken dump func.

--- turbo/log.lua
+++ turbo/log.lua
@@ -25,9 +25,11 @@ local ffi = require "ffi"
 
 local log = {} -- log namespace.
 
+
 log.stringify = function (t, name, indent)
    local cart     -- a container
    local autoref  -- for self references
+
    local function isemptytable(t) return next(t) == nil end
    local function basicSerialize (o)
 	  local so = tostring(o)
@@ -48,41 +50,39 @@ log.stringify = function (t, name, indent)
 	 return string.format("%q", so)
 	  end
    end
-
-   local function addtocart (value, name, indent, saved, field)
-	  indent = indent or ""
-	  saved = saved or {}
-	  field = field or name
-
-	  cart = cart .. indent .. field
-
-	  if type(value) ~= "table" then
+    local function addtocart (value, name, indent, saved, field)
+        indent = indent or ""
+        saved = saved or {}
+        field = field or name
+    
+        cart = cart .. indent .. field
+    
+        if type(value) ~= "table" then
             cart = cart .. " = " .. basicSerialize(value) .. ";\n"
-	  else
-	 if saved[value] then
-		cart = cart .. " = {}; -- " .. saved[value]
-			.. " (self reference)\n"
-		autoref = autoref ..  name .. " = " .. saved[value] .. ";\n"
-	 else
-		saved[value] = name
-		--if tablecount(value) == 0 then
-		if isemptytable(value) then
-		   cart = cart .. " = {};\n"
-		else
-		   cart = cart .. " = {\n"
-		   for k, v in pairs(value) do
-		  k = basicSerialize(k)
-		  local fname = string.format("%s[%s]", name, k)
-		  field = string.format("[%s]", k)
-		  -- three spaces between levels
-		  addtocart(v, fname, indent .. "   ", saved, field)
-		   end
-		   cart = cart .. indent .. "};\n"
-		end
-	 end
+        else
+            if saved[value] then
+                cart = cart .. " = {}; -- " .. saved[value]
+                    .. " (self reference)\n"
+                autoref = autoref ..  name .. " = " .. saved[value] .. ";\n"
+            else
+                saved[value] = name
+                --if tablecount(value) == 0 then
+                if isemptytable(value) then
+                    cart = cart .. " = {};\n"
+                else
+                    cart = cart .. " = {\n"
+                    for k, v in pairs(value) do
+                        k = basicSerialize(k)
+                        local fname = string.format("%s[%s]", name, k)
+                        field = string.format("[%s]", k)
+                        -- three spaces between levels
+                        addtocart(v, fname, indent .. "   ", saved, field)
+                    end
+                    cart = cart .. indent .. "};\n"
+                end
+            end
         end
-   end
-
+    end
    name = name or "__unnamed__"
    if type(t) ~= "table" then
 	  return name .. " = " .. basicSerialize(t)
