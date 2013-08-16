995c8dfb5e35ac03f6f0664ec489423a6e8e4e44
John Abrahamsen <jhnabrhmsn@gmail.com>
2013-08-16 17:05:39 +0200

    strfind test added.

--- tests/unittests/strfind.lua
+++ tests/unittests/strfind.lua
@@ -15,21 +15,64 @@
 -- limitations under the License.
 
 local turbo = require "turbo"
+local ffi = require "ffi"
 
 describe("turbo low level buffer module", function()
+	local stress = false
+	local search_time = 1000000
+	local sneedle = "\r\n\r\n"
+	local shaystack = "pkl2''234kokosmv,.-sd,fkwerj234982a9dfj89as9dhrh234"
+	
 	it("should work", function()
 		local haystack = turbo.structs.buffer()
-		for i = 0, 10000 do 
-			haystack:append_luastr_right("this is a big ass string.")
+		for i = 0, search_time do 
+			haystack:append_luastr_right(shaystack)
 		end
-		haystack:append_luastr_right("noob")
-		for i = 0, 10000 do 
-			haystack:append_luastr_right("this is a big ass string.")
+		haystack:append_luastr_right(sneedle)
+		for i = 0, search_time do 
+			haystack:append_luastr_right(shaystack)
 		end
 		local needle = turbo.structs.buffer()
-		needle:append_luastr_right("string")
+		needle:append_luastr_right(sneedle)
 		local h_str, h_len = haystack:get()
 		local n_str, n_len = needle:get()
-		print(turbo.util.TBM(n_str, n_len, h_str, h_len))
+		assert.equal(turbo.util.str_find(h_str, n_str, h_len, n_len) - h_str, 51000051)
+		if stress then
+			for i=0, 1000 do
+				assert.equal(turbo.util.str_find(h_str, n_str, h_len, n_len) - h_str, 51000051)
+			end
+		end
+	end)
+
+	it("turbo.util.str.find should perform on par with string.find", function()
+		local haystack = turbo.structs.buffer()
+		for i = 0, search_time do 
+			haystack:append_luastr_right(shaystack)
+		end
+		haystack:append_luastr_right(sneedle)
+		for i = 0, search_time do 
+			haystack:append_luastr_right(shaystack)
+		end
+		local needle = turbo.structs.buffer()
+		needle:append_luastr_right(sneedle)
+		local h_str, h_len = haystack:get()
+		local n_str, n_len = needle:get()
+		local start = turbo.util.gettimeofday()
+		turbo.util.str_find(h_str, n_str, h_len, n_len)
+		local ag_time = turbo.util.gettimeofday() - start
+		-- Lua comparison.
+		local str = {}
+		for i = 0, search_time do 
+			str[#str+1] = shaystack
+		end
+		str[#str+1] = sneedle
+		for i = 0, search_time do 
+			str[#str+1] = shaystack
+		end
+		str = table.concat(str)
+		start = turbo.util.gettimeofday()
+		str:find(sneedle, 1, true)
+		local find_time = turbo.util.gettimeofday() - start
+		print("\nturbo.util.str_find searched in " .. ag_time .. "ms and string.find in " .. find_time .. "ms.")
 	end)
 end)
