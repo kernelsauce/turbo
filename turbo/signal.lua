f210c256ccb1486ffb074010d06dad2a5f8e4d81
Espen Kåsa Notodden <enotodden@puvoid.com>
2013-11-11 00:29:18 +0100

    indent fixes..

--- turbo/signal.lua
+++ turbo/signal.lua
@@ -37,9 +37,9 @@ return {
     , SIG_UNBLOCK = 1
     , SIG_SETMASK = 2
     -- Fake signal functions.
-    , SIG_ERR =    ffi.cast("sighandler_t", -1)    --[[ Error return.  ]]
-    , SIG_DFL =    ffi.cast("sighandler_t", 0)    --[[ Default action.  ]]
-    , SIG_IGN =    ffi.cast("sighandler_t", 1)    --[[ Ignore signal.  ]]
+    , SIG_ERR = ffi.cast("sighandler_t", -1)    --[[ Error return.  ]]
+    , SIG_DFL = ffi.cast("sighandler_t", 0)     --[[ Default action.  ]]
+    , SIG_IGN = ffi.cast("sighandler_t", 1)     --[[ Ignore signal.  ]]
     -- Signals.
     , SIGHUP    =   1       --[[ Hangup (POSIX).  ]]
     , SIGINT    =   2       --[[ Interrupt (ANSI).  ]]
