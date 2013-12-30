f3bb6e1bb22fc5546b3e2cc640d8f264a6f94f7a
John Abrahamsen <jhnabrhmsn@gmail.com>
2013-12-30 19:25:28 +0100

    Fixed regression for single cookie.

--- turbo/web.lua
+++ turbo/web.lua
@@ -555,14 +555,14 @@ function web.RequestHandler:_parse_cookies()
     if cnt == 0 then
         return
     elseif cnt == 1 then
-        for key, value in cookie_str:gmatch("([%a%c%w%p]+)=([%a%c%w%p]+);") do
-            cookies[escape.unescape(key)] = escape.unescape(value)        
+        for key, value in cookie_str:gmatch("([%a%c%w%p]+)=([%a%c%w%p]+);?") do
+            cookies[escape.unescape(key)] = escape.unescape(value)
         end
     elseif cnt > 1 then
         for i = 1, cnt do
             for key, value in 
-                cookie_str[i]:gmatch("([%a%c%w%p]+)=([%a%c%w%p]+);") do
-                cookies[escape.unescape(key)] = escape.unescape(value)        
+                cookie_str[i]:gmatch("([%a%c%w%p]+)=([%a%c%w%p]+);?") do
+                cookies[escape.unescape(key)] = escape.unescape(value)
             end
         end
     end
