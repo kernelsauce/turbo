6e6af9e75485618dc92c4ce67802d42b64f00286
John Abrahamsen <jhnabrhmsn@gmail.com>
2014-01-16 22:54:52 +0100

    Tabs to spaces.

--- examples/chatapp/chatapp.lua
+++ examples/chatapp/chatapp.lua
@@ -20,72 +20,72 @@ local turbo = require "turbo"
 local ChatRoom = class("ChatRoom")
 
 function ChatRoom:initialize()
-	self.persons = {}
-	self.subscribers = {}
+    self.persons = {}
+    self.subscribers = {}
 end
 
 function ChatRoom:broadcast(package)
-	local time = turbo.util.gettimeofday()
-	for i = 1, #self.subscribers do
-		local sub = self.subscribers[i]
-		sub.socket:write_message(turbo.escape.json_encode {
-			package = package,
-			time = time
-		})
-	end
+    local time = turbo.util.gettimeofday()
+    for i = 1, #self.subscribers do
+        local sub = self.subscribers[i]
+        sub.socket:write_message(turbo.escape.json_encode {
+            package = package,
+            time = time
+        })
+    end
 end
 
 function ChatRoom:add(nick)
-	for i=1, #self.persons do
-		if self.persons[i].nick == nick then
-			return false
-		end
-	end
-	self.persons[#self.persons+1] = {nick=nick}
-	return true
+    for i=1, #self.persons do
+        if self.persons[i].nick == nick then
+            return false
+        end
+    end
+    self.persons[#self.persons+1] = {nick=nick}
+    return true
 end
 
 function ChatRoom:is_registered(nick)
-	for i=1, #self.persons do
-		if self.persons[i].nick == nick then
-			return true
-		end
-	end
+    for i=1, #self.persons do
+        if self.persons[i].nick == nick then
+            return true
+        end
+    end
 end
 
 function ChatRoom:subscribe(nick, socket)
-	table.insert(self.subscribers, {nick=nick, socket=socket})
+    table.insert(self.subscribers, {nick=nick, socket=socket})
 
-	-- Send a partcipant joined message.
-	self:broadcast({
-		type = "participant-joined",
-		data = nick
-	})
+    -- Send a partcipant joined message.
+    self:broadcast({
+        type = "participant-joined",
+        data = nick
+    })
 
-	-- Send a updated participant list.
-	local nicks = {}
-	for i = 1, #self.subscribers do 
-		nicks[#nicks+1] = self.subscribers[i].nick
-	end
-	self:broadcast({
-		type = "participant-update",
-		data = nicks
-	})
+    -- Send a updated participant list.
+    local nicks = {}
+    for i = 1, #self.subscribers do 
+        nicks[#nicks+1] = self.subscribers[i].nick
+    end
+    self:broadcast({
+        type = "participant-update",
+        data = nicks
+    })
 end
 
 function ChatRoom:unsubscribe(nick, socket)
-	-- NYI, remove nick and socket object.
-	for i = 1, #self.subscribers do 
-		if self.subscribers[i].nick == nick then
-			assert(self.subscribers[i].socket == socket,
-				   "Uh oh...")
-			table.remove(self.subscribers, i)
-		end
-	end
-	self:broadcast({
-		type = "participant-left",
-		data = nick
-	})
+    -- NYI, remove nick and socket object.
+    for i = 1, #self.subscribers do 
+        if self.subscribers[i].nick == nick then
+            assert(self.subscribers[i].socket == socket,
+                   "Uh oh...")
+            table.remove(self.subscribers, i)
+        end
+    end
+    self:broadcast({
+        type = "participant-left",
+        data = nick
+    })
 end
 
 
@@ -93,79 +93,79 @@ end
 local ChatCom = class("ChatCom", turbo.websocket.WebSocketHandler)
 
 function ChatCom:prepare()
-	-- Prepare method is run before you enter WebSocket protocol land.
-	-- You can then discard requests based on e.g cookies.
-	self.nick = self:get_secure_cookie("nick")
-	if not self.nick then
-		-- User has not signed in.
-		error(turbo.web.HTTPError(400))
-	end
-	if not self.options.chatroom:is_registered(self.nick) then
-		error(turbo.web.HTTPError(400))
-	end
+    -- Prepare method is run before you enter WebSocket protocol land.
+    -- You can then discard requests based on e.g cookies.
+    self.nick = self:get_secure_cookie("nick")
+    if not self.nick then
+        -- User has not signed in.
+        error(turbo.web.HTTPError(400))
+    end
+    if not self.options.chatroom:is_registered(self.nick) then
+        error(turbo.web.HTTPError(400))
+    end
 end
 
 function ChatCom:open()
-	self.options.chatroom:subscribe(self.nick, self)
+    self.options.chatroom:subscribe(self.nick, self)
 end
 
 function ChatCom:on_message(msg)
-	local pack = turbo.escape.json_decode(msg).package
-	assert(pack, "Invalid ChatCom application protocol.")
+    local pack = turbo.escape.json_decode(msg).package
+    assert(pack, "Invalid ChatCom application protocol.")
 
     self.options.chatroom:broadcast({
-    	type = "message",
-    	data = {
-    		nick = self.nick, 
-    		msg = pack.data.msg
-    	}
+        type = "message",
+        data = {
+            nick = self.nick, 
+            msg = pack.data.msg
+        }
     })
 end
 
 function ChatCom:on_close()
-	self.options.chatroom:unsubscribe(self.nick, self)
-	turbo.log.notice("User disconnected: " .. self.nick)
+    self.options.chatroom:unsubscribe(self.nick, self)
+    turbo.log.notice("User disconnected: " .. self.nick)
 end
 
 local SignInHandler = class("SignInHandler", turbo.web.RequestHandler)
 
 --- Handler to check if user is already logged in.
 function SignInHandler:get()
-	local nick = self:get_argument("nick")
-	if not self.options.chatroom:is_registered(nick) then
-		self:write({ok = false})
-		return
-	end
-	self:write({ok = true})
+    local nick = self:get_argument("nick")
+    if not self.options.chatroom:is_registered(nick) then
+        self:write({ok = false})
+        return
+    end
+    self:write({ok = true})
 end
 
 --- Handler to register nickname etc.
 function SignInHandler:post()
-	local nick = self:get_argument("nick")
-	if not self.options.chatroom:add(nick) then
-		self:write({ok = false})
-		return
-	end
-	self:set_secure_cookie("nick", nick)
-	self:write({ok = true})
+    local nick = self:get_argument("nick")
+    if not self.options.chatroom:add(nick) then
+        self:write({ok = false})
+        return
+    end
+    self:set_secure_cookie("nick", nick)
+    self:write({ok = true})
 end
 
 
 local function main()
-	local chatroom = ChatRoom()
-	local app = turbo.web.Application(
-	{
-		{"^/chatcom$", ChatCom, {chatroom = chatroom}},
-		{"^/signin$", SignInHandler, {chatroom = chatroom}},
-		{"^/$", turbo.web.StaticFileHandler, "assets/index.html"},
-		{"^/assets/(.*)$", turbo.web.StaticFileHandler, "assets/"}
-	}, 
-	{
-		cookie_secret = "akofdkapokfposakfdsafasdåpfkadåpf"
-	})
+    local chatroom = ChatRoom()
+    local app = turbo.web.Application(
+    {
+        {"^/chatcom$", ChatCom, {chatroom = chatroom}},
+        {"^/signin$", SignInHandler, {chatroom = chatroom}},
+        {"^/$", turbo.web.StaticFileHandler, "assets/index.html"},
+        {"^/assets/(.*)$", turbo.web.StaticFileHandler, "assets/"}
+    }, 
+    {
+        cookie_secret = "akofdkapokfposakfdsafasdåpfkadåpf"
+    })
 
-	app:listen(8888)
-	turbo.ioloop.instance():start()
+    app:listen(8888)
+    turbo.ioloop.instance():start()
 end
 
 main()
