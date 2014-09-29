802ecbc8c0b7a1bbbb03333724d14075e10d1ccf
Michal Kolodziejczyk <miko@voicelab.pl>
2014-09-29 11:56:26 +0200

    Added lovebird integration example

---
+++ examples/helloworld-lovebird.lua
@@ -0,0 +1,34 @@
+--- Turbo.lua Hello world example
+--
+-- Copyright 2013 John Abrahamsen
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+-- http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+local turbo = require "turbo"
+
+local ExampleHandler = class("ExampleHandler", turbo.web.RequestHandler)
+
+function ExampleHandler:get()
+    self:write("Hello World!")
+end
+
+-- Requires https://github.com/rxi/lovebird and luasocket
+local lovebird = require 'lovebird'
+turbo.ioloop.instance():set_interval(100, function () 
+  lovebird.update()
+  return true
+end)
+
+
+turbo.web.Application({{"^/$", ExampleHandler}}):listen(8888)
+turbo.ioloop.instance():start()
