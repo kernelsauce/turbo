c551cb37dafbdc313e24e2a98f588e2564e5b367
John Abrahamsen <jhnabrhmsn@gmail.com>
2015-03-30 19:47:45 +0200

    Removed forced LuaSocket usage in chatapp example.

--- examples/chatapp/chatapp.lua
+++ examples/chatapp/chatapp.lua
@@ -14,7 +14,6 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
-_G.__TURBO_USE_LUASOCKET__ = true
 _G.TURBO_SSL = true
 local turbo = require "turbo"
 
