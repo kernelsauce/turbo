13a8a6c20e6c9b74276142aa4cddd1fc1403c690
John Abrahamsen <jhnabrhmsn@gmail.com>
2015-04-02 22:40:12 +0200

    Changed test dir. layout.

--- tests/run_all_test.lua
+++
@@ -1,9 +0,0 @@
-TURBO_SSL = true
-require "unittests.structs"
-require "unittests.httputil"
-require "unittests.iostream"
-require "unittests.util"
-require "unittests.mustache"
-require "unittests.async"
-require "unittests.hash"
-require "unittests.web"
