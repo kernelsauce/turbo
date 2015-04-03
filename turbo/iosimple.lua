4dc09fb5abf0061a239ae0bd78b99848b1c7a6fa
John Abrahamsen <jhnabrhmsn@gmail.com>
2015-04-03 22:59:58 +0200

    IOSimple works nicely.

--- turbo/iosimple.lua
+++ turbo/iosimple.lua
@@ -20,6 +20,7 @@ local log =         require "turbo.log"
 local ioloop =      require "turbo.ioloop"
 local deque =       require "turbo.structs.deque"
 local buffer =      require "turbo.structs.buffer"
+local coctx =       require "turbo.coctx"
 local socket =      require "turbo.socket_ffi"
 local sockutils =   require "turbo.sockutil"
 local util =        require "turbo.util"
@@ -33,12 +34,13 @@ local iosimple = {} -- iosimple namespace
 
 function iosimple.dial(address, io)
 	assert(type(address) == "string", "No address in call to dial.")
-	local protocol, host, port = address:match("^(%a+)://(.+)")
-
+	local protocol, host, port = address:match("^(%a+)://(.+):(%d+)")
+	port = tonumber(port)
 	assert(
 		protocol and host,
 		"Invalid address. Use e.g \"tcp://turbolua.org:8080\".")
 
+	io = io or ioloop.instance()
 	local sock_t
 	local address_family
 	if protocol == "tcp" then
@@ -61,24 +63,24 @@ function iosimple.dial(address, io)
     if sock == -1 then
         error("Could not create socket.")
     end
-    local stream = iostream.IOStream(host)
+    local stream = iostream.IOStream(sock)
     local ctx = coctx.CoroutineContext(io)
     
     stream:connect(host, port, address_family, 
     	function()
-    		ctx:set_argument({1})
+    		ctx:set_arguments({true})
     		ctx:finalize_context()
 		end,
 		function(err)
-			ctx:set_argument({0, sockerr, strerr})
+			ctx:set_arguments({false, sockerr, strerr})
 			ctx:finalize_context()
     	end
     )
     local rc, sockerr, strerr = coroutine.yield(ctx)
-    if rc ~= 0 then
+    if rc ~= true then
     	error(string.format("Could not connect to %s, %s", address, strerr))
     end
-    return iosimple.IOSimple(stream)
+    return iosimple.IOSimple(stream, io)
 end
 
 iosimple.IOSimple = class("IOSimple")
@@ -86,40 +88,59 @@ iosimple.IOSimple = class("IOSimple")
 function iosimple.IOSimple:initialize(stream, io)
 	self.stream = stream
 	self.io = io
+	self.stream:set_close_callback(self._wake_yield_close, self)
+end
+
+function iosimple.IOSimple:_wake_yield_close(...)
+	if self.coctx then
+		ctx:set_arguments({nil, "disconnected"})
+		ctx:finalize_context()	
+	end
 end
 
 function iosimple.IOSimple:_wake_yield(...)
 	local ctx = self.coctx
 	self.coctx = nil
-	ctx:set_argument({...})
+	ctx:set_arguments({...})
 	ctx:finalize_context()
+end
+
+function iosimple.IOSimple:close(str)
+	self.stream:close()
 end
 
 function iosimple.IOSimple:write(str)
 	assert(not self.coctx, "IOSimple is already working.")
 	self.coctx = coctx.CoroutineContext(self.io)
-	self.iostream:write(str, self._wake_yield, self)
+	self.stream:write(str, self._wake_yield, self)
 	return coroutine.yield(self.coctx)
 end
 
 function iosimple.IOSimple:read_until(delimiter)
 	assert(not self.coctx, "IOSimple is already working.")
 	self.coctx = coctx.CoroutineContext(self.io)
-	self.iostream:read_until(delimiter, self._wake_yield, self)
+	self.stream:read_until(delimiter, self._wake_yield, self)
+	return coroutine.yield(self.coctx)
+end
+
+function iosimple.IOSimple:read_bytes(bytes)
+	assert(not self.coctx, "IOSimple is already working.")
+	self.coctx = coctx.CoroutineContext(self.io)
+	self.stream:read_bytes(bytes, self._wake_yield, self)
 	return coroutine.yield(self.coctx)
 end
 
 function iosimple.IOSimple:read_until_pattern(pattern)
 	assert(not self.coctx, "IOSimple is already working.")
 	self.coctx = coctx.CoroutineContext(self.io)
-	self.iostream:read_until_pattern(pattern, self._wake_yield, self)
+	self.stream:read_until_pattern(pattern, self._wake_yield, self)
 	return coroutine.yield(self.coctx)
 end
 
-function iosimple.IOSimple:read_until_close(pattern)
+function iosimple.IOSimple:read_until_close()
 	assert(not self.coctx, "IOSimple is already working.")
 	self.coctx = coctx.CoroutineContext(self.io)
-	self.iostream:read_until_close(self._wake_yield, self)
+	self.stream:read_until_close(self._wake_yield, self)
 	return coroutine.yield(self.coctx)
 end
 
