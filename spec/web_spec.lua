13cb0b75f1e16f89f7f34468303458fabc2c1ca6
John Abrahamsen <jhnabrhmsn@gmail.com>
2015-04-03 12:04:11 +0200

    Added forced LuaSocket tests to travis builds.

--- spec/web_spec.lua
+++ spec/web_spec.lua
@@ -14,6 +14,7 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
+_G.__TURBO_USE_LUASOCKET__ = os.getenv("TURBO_USE_LUASOCKET") and true or false
 _G.TURBO_SSL = true
 local turbo = require "turbo"
 math.randomseed(turbo.util.gettimeofday())
