4fc63215ed4da87e40955303b163e54706b2e91b
John Abrahamsen <jhnabrhmsn@gmail.com>
2015-10-20 19:44:07 +0200

    Changed file header to not contain the old MIT license and incorrect URL for project.

--- deps/turbo_ffi_wrap.h
+++ deps/turbo_ffi_wrap.h
@@ -1,30 +1,19 @@
-/* Wrapper for FFI calls from Turbo.lua, where its is difficult because
+/* Wrapper for FFI calls where its is difficult because
 of long header file, macros, define's etc.
 
-Copyright 2013 John Abrahamsen < JhnAbrhmsn@gmail.com >
+Copyright John Abrahamsen 2011 - 2015
 
-This module "http_parser_ffi_wrap" is a part of the Turbo Web server.
-For the complete stack hereby called "software package" please see:
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-https://github.com/JohnAbrahamsen/turbo-ng/
+http://www.apache.org/licenses/LICENSE-2.0
 
-"Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-of the Software, and to permit persons to whom the Software is furnished to do
-so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE."          */
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
 
 #include <stdint.h>
 #include <stdlib.h>
