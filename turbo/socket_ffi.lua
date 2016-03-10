37fa3ad43795be7b76671c128fd27c683e9d3d4b
Wojciech Nizinski <w.nizinski@grinn-global.com>
2016-03-10 13:07:07 +0100

    iostream: reread resolv.conf if getaddrinfo() returns EAI_AGAIN

--- turbo/socket_ffi.lua
+++ turbo/socket_ffi.lua
@@ -288,7 +288,8 @@ E = {
     EWOULDBLOCK =       11,
     EINPROGRESS =       150,
     ECONNRESET =        131,
-    EPIPE =             32
+    EPIPE =             32,
+    EAI_AGAIN =         3
 }
 else
 E = {
@@ -296,7 +297,8 @@ E = {
     EWOULDBLOCK =       11,
     EINPROGRESS =       115,
     ECONNRESET =        104,
-    EPIPE =             32
+    EPIPE =             32,
+    EAI_AGAIN =         3
 }
 end
 
