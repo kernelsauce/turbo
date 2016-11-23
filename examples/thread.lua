87e5e6ef4610920ddfbf3e5a20f240eb34896c73
John Abrahamsen <jhnabrhmsn@gmail.com>
2016-11-23 19:09:02 +0100

    Fixed threading example.

--- examples/thread.lua
+++ examples/thread.lua
@@ -16,31 +16,16 @@
 
 local turbo = require "turbo"
 
-local function hard_work(n)
-    return "This is hard work!"
-end
-
 
 turbo.ioloop.instance():add_callback(function()
 
     local thread = turbo.thread.Thread(function(th)
-        th:send(hard_work())
+        th:send("Hello World.")
         th:stop()
     end)
 
-    local thread2 = turbo.thread.Thread(function(th)
-        th:send(hard_work())
-        th:stop()
-    end)
-
-    local data = coroutine.yield(turbo.async.task(
-        thread.wait_for_data, thread))
-    print(data)
-
-    local data2 = coroutine.yield(turbo.async.task(
-        thread2.wait_for_data, thread2))
-    print(data2)
-
+    print(thread:wait_for_data())
+    thread:wait_for_finish()
     turbo.ioloop.instance():close()
 
 end):start()
