ba2134ca51208f6d24fab5410315e8e3d6089001
John Abrahamsen <jhnabrhmsn@gmail.com>
2016-11-23 12:55:15 +0100

    Small added CoroutineContext thing.

--- turbo/thread.lua
+++ turbo/thread.lua
@@ -21,6 +21,7 @@ local log = require "turbo.log"
 local socket = require "turbo.socket_ffi"
 local sockutil = require "turbo.sockutil"
 local iostream = require "turbo.iostream"
+local coctx = require "turbo.coctx"
 require "turbo.cdef"
 
 local thread = {}
@@ -41,7 +42,6 @@ function thread.Thread:stop()
         -- Send stop message over wire.
         self.pipe:write("STOP\r\n\r\n", function()
             ffi.C.wait(nil)
-            os.remove(self.com_port)
         end)
     else
         -- Stop thread as child.
@@ -49,6 +49,7 @@ function thread.Thread:stop()
         self.pipe:write("STOP\r\n\r\n", function()
                 _self.pipe:close()
                 _self.io_loop:close()
+                os.remove(self.com_port)
                 os.exit(0)
             end
         )
@@ -57,11 +58,23 @@ end
 
 --- Called by either main or child thread to send data to each other.
 function thread.Thread:send(data)
-    self.pipe:write("DATA\r\n\r\n"..data:len().."\r\n\r\n"..data)
+    self.pipe:write("DATA\r\n\r\n"..data:len().."\r\n\r\n"..data, callback, callback_arg)
 end
 
--- Wait for data to become available from child thread.
+--- Wait for data to become available from child thread. Takes either a
+-- callback or can be yielded directly to IOLoop.
+-- @param callback (Function) Optional callback function.
+-- @param arg Optional argument for callback. If arg is given then it will
+-- be the first argument for the callback and the data will be the second.
+-- @return (CoroutineContext) Yieldable object if no callback is given
 function thread.Thread:wait_for_data(callback, callback_arg)
+    if not callback then
+        -- Not using callbacks, return a CoroutineContext instance instead.
+        self.ctx = coctx.CoroutineContext(self.io_loop)
+        callback = coctx.CoroutineContext.finalize_context
+        callback_arg = self.ctx
+    end
+
     if self._waiting_data then
         -- Data is already available.
         local data = self._waiting_data
@@ -70,6 +83,9 @@ function thread.Thread:wait_for_data(callback, callback_arg)
             -- Can not run callback directly. CoroutineContext not on
             -- stack if async.task is used.
             if callback_arg then
+                if self.ctx then
+                    self.ctx:set_arguments(data)
+                end
                 callback(callback_arg, data)
             else
                 callback(data)
@@ -84,6 +100,58 @@ function thread.Thread:wait_for_data(callback, callback_arg)
         self.child_thread_data_cb = callback
         self.child_thread_data_arg = callback_arg
     end
+    return self.ctx
+end
+
+--- Wait for thread to stop running. Takes either a
+-- callback or can be yielded directly to IOLoop.
+-- @param callback (Function) Optional callback function.
+-- @param arg Optional argument for callback. If arg is given then it will
+-- be the first argument for the callback and the data will be the second.
+-- @return (CoroutineContext) Yieldable object if no callback is given
+function thread.Thread:wait_for_finish(callback, callback_arg)
+    if not callback then
+        self.ctx = coctx.CoroutineContext(self.io_loop)
+        callback = coctx.CoroutineContext.finalize_context
+        callback_arg = self.ctx
+    end
+    if not self.running then
+        self.io_loop:add_callback(function()
+            callback(callback_arg)
+        end)
+        return
+    end
+    self.args.exit_callback = callback
+    self.args.exit_callback_arg = callback_arg
+    return self.ctx
+end
+
+--- Wait for thread pipe to be connected. Must be used by main thread
+-- before attempting to send data to child. Takes either a
+-- callback or can be yielded directly to IOLoop.
+-- @param callback (Function) Optional callback function.
+-- @param arg Optional argument for callback. If arg is given then it will
+-- be the first argument for the callback and the data will be the second.
+-- @return (CoroutineContext) Yieldable object if no callback is given
+function thread.Thread:wait_for_pipe(callback, callback_arg)
+    if not self.main_thread then
+        error(
+            "Child thread does not need to wait for pipe.")
+    end
+    if not callback then
+        self.ctx = coctx.CoroutineContext(self.io_loop)
+        callback = coctx.CoroutineContext.finalize_context
+        callback_arg = self.ctx
+    end
+    if self.connected then
+        self.io_loop:add_callback(function()
+            callback(callback_arg)
+        end)
+        return
+    end
+    self.args.connect_callback = callback
+    self.args.connect_callback_arg = callback_arg
+    return self.ctx
 end
 
 function thread.Thread:_run_thread()
@@ -102,6 +170,7 @@ function thread.Thread:_run_thread()
     end
     -- New thread.
     self.io_loop = ioloop.IOLoop()
+    _G.io_loop_instance = self.io_loop
     self.main_thread = false
     self:_connect_to_main_thread()
 end
@@ -110,29 +179,44 @@ function thread.Thread:_execute_thread_func()
     xpcall(self._child_func, function(err) 
         -- Child thread encountered error.
         -- Cleanup everything and exit thread.
-        log.error("[thread.lua] Child thread error: " .. err)
-        self.stop()
+        local thread = tonumber(ffi.C.getpid())
+        local trace = debug.traceback(coroutine.running(), err, 2)
+        local _str_borders_down = string.rep("▼", 80)
+        local _str_borders_up = string.rep("▲", 80)
+
+        log.error(
+            string.format(
+            "[thread.lua] Error in thread. PID %s is dead.\n%s\n%s\n%s\n",
+            thread,
+            _str_borders_down,
+            trace,
+            _str_borders_up))
+
+        self:stop()
     end,
     self)
 end
 
 function thread.Thread:_connect_to_main_thread()
+    local errno, rc
     local client_address = ffi.new("struct sockaddr_un");
+
     client_address.sun_family = socket.AF_UNIX;
     ffi.copy(client_address.sun_path, 
         ffi.cast("const char*", self.com_port),
         math.min(self.com_port:len(), ffi.sizeof(client_address.sun_path)))
     self.client_sockfd = ffi.C.socket(socket.AF_UNIX, socket.SOCK_STREAM, 0);
     if self.client_sockfd < 0 then
+        errno = ffi.errno()
         log.error(string.format(
             "[thread.lua ]Errno %d. Could not create Unix socket FD. %s",
             errno,
             socket.strerror(errno)))
         os.exit(1)
     end
-    local rc = ffi.C.connect(self.client_sockfd,
-                             ffi.cast("const struct sockaddr *", client_address),
-                             ffi.sizeof(client_address))
+    rc = ffi.C.connect(self.client_sockfd,
+                       ffi.cast("const struct sockaddr *", client_address),
+                       ffi.sizeof(client_address))
     if rc ~= 0 then
         -- Main thread probably has not created socket yet. Wait.
         -- Should be done more elegantly...
@@ -144,7 +228,7 @@ function thread.Thread:_connect_to_main_thread()
     if rc ~= 0 then
         errno = ffi.errno()
         log.error(string.format(
-            "[thread.lua ]Errno %d. Could not connect to main thread pipe. %s",
+            "[thread.lua] Errno %d. Could not connect to main thread pipe. %s",
             errno,
             socket.strerror(errno)))
         ffi.C.close(self.client_sockfd)
@@ -208,10 +292,20 @@ end
 --- Called when child thread connects to main thread.
 function thread.Thread:_child_connects(fd)
     local pipe = iostream.IOStream(fd, self.io_loop)
+    self.connected = true
+    if self.args.connect_callback then
+        local callback = self.args.connect_callback
+        local arg = self.args.connect_callback_arg
+        self.args.connect_callback = nil
+        self.args.connect_callback_arg = nil
+        self.io_loop:add_callback(function()
+            callback(arg)
+        end)
+    end
     self.pipe = pipe
     pipe:read_until("\r\n\r\n",
-                             thread.Thread._child_command_sent,
-                             self)
+                    thread.Thread._child_command_sent,
+                    self)
 end
 
 --- Called when child thread is sending data over pipe.
@@ -226,6 +320,9 @@ function thread.Thread:_child_data_sent(data)
         self.main_thread_data_cb = nil
         self.main_thread_data_arg = nil
         if arg then
+            if self.ctx then
+                self.ctx:set_arguments(data)
+            end
             cb(arg, data)
         else
             cb(data)
@@ -260,6 +357,7 @@ function thread.Thread:_child_command_sent(cmd)
             end)
             _self.pipe:close()
             ffi.C.close(_self.server_sockfd)
+            self.running = false
         end
         -- Collect thread.
         ffi.C.wait(nil)
@@ -273,6 +371,9 @@ function thread.Thread:_main_data_sent(data)
     self.child_thread_data_cb = nil
     self.child_thread_data_arg = nil
     if arg then
+        if self.ctx then
+            self.ctx:set_arguments(data)
+        end
         cb(arg, data)
     else
         cb(data)
