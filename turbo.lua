3b773d61a584a614ea1f854a2326e94c1212899a
John Abrahamsen <jhnabrhmsn@gmail.com>
2018-01-26 18:24:45 +0100

    Add rockspec for 2.1.2 and bump version.

--- turbo.lua
+++ turbo.lua
@@ -65,10 +65,10 @@ local turbo = {}  -- turbo main namespace.
 -- changes, the major version is incremented and others are set to zero.
 turbo.MAJOR_VERSION = 2
 turbo.MINOR_VERSION = 1
-turbo.MICRO_VERSION = 1
+turbo.MICRO_VERSION = 2
 -- A 3-byte hexadecimal representation of the version, e.g. 0x010201 for
 -- version 1.2.1 and 0x010300 for version 1.3.
-turbo.VERSION_HEX = 0x020101
+turbo.VERSION_HEX = 0x020102
 if turbo.MICRO_VERSION then
     turbo.VERSION = string.format("%d.%d.%d",
         turbo.MAJOR_VERSION,
